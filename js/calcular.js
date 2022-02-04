aa565b79d7ee2344f496ef2cc7df327ad0cb96f8
Victor Adão da Silva <70406400+VictorSilva27@users.noreply.github.com>
2022-02-04 07:52:14 -0300

    Initial commit

---
+++ js/calcular.js
@@ -0,0 +1,39 @@
+function Adicao(){
+                    
+    var num1 = document.getElementById('num1').value;
+    var num2 = document.getElementById('num2').value;
+
+    var resultado = parseInt(num1) + parseInt(num2);
+
+    document.getElementById('resultado').innerText = resultado;
+}
+
+function Subtracao(){
+                    
+    var num1 = document.getElementById('num1').value;
+    var num2 = document.getElementById('num2').value;
+
+    var resultado = parseInt(num1) - parseInt(num2);
+
+    document.getElementById('resultado').innerText = resultado;
+}
+
+function Multiplicacao(){
+                    
+    var num1 = document.getElementById('num1').value;
+    var num2 = document.getElementById('num2').value;
+
+    var resultado = parseInt(num1) * parseInt(num2);
+
+    document.getElementById('resultado').innerText = resultado;
+}
+
+function Divisao(){
+                    
+    var num1 = document.getElementById('num1').value;
+    var num2 = document.getElementById('num2').value;
+
+    var resultado = parseInt(num1) / parseInt(num2);
+
+    document.getElementById('resultado').innerText = resultado;
+}
