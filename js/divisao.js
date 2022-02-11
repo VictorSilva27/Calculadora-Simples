d3bcf3f3d9eae2c1612d60a7e7926fb5b9a2d113
Victor Adão da Silva <70406400+VictorSilva27@users.noreply.github.com>
2022-02-11 17:39:38 -0300

    oficial

--- js/divisao.js
+++ js/divisao.js
@@ -1,4 +1,13 @@
 function Dividir(numero_1, numero_2){
     
-    document.getElementById('resultado').innerText =  numero_1 / numero_2;
+    if(numero_2 == "0"){
+
+        document.getElementById('resultado').innerText = "Impossivel";
+        alert("Impossível dividir por 0");
+        
+    }else{
+
+        document.getElementById('resultado').innerText =  numero_1 / numero_2;
+
+    }   
 }
